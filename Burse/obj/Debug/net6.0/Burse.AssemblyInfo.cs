c885942e8e903afd9b5b2c195073b52d199b975f
Staniislaw <gratiis6@gmail.com>
2025-04-05 18:05:51 +0300

    Cleanup .vs and build artifacts

--- Burse/obj/Debug/net6.0/Burse.AssemblyInfo.cs
+++ Burse/obj/Debug/net6.0/Burse.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Burse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f92d11eb5a00db87819fbeb58832ab80be159f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb9e6fe6b317eb3d5ade2cbc043ad5002ae30627")]
 [assembly: System.Reflection.AssemblyProductAttribute("Burse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Burse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
